3ba6a8bd2b0bb9ae739cd0bdc304d1470f05eeb1
guillaumekrvr2 <guillaumekervran@gmail.com>
2025-05-02 17:26:31 +0200

    feat: add native stack navigator with smooth transition

--- navigation/HomeStackNavigator.jsx
+++ navigation/HomeStackNavigator.jsx
@@ -1,16 +1,18 @@
 // navigation/HomeStackNavigator.jsx
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createNativeStackNavigator, TransitionPresets } from '@react-navigation/stack'
 import HomeScreen from '../screens/HomeScreen'
 import NumbersScreen from '../screens/NumbersScreen'
 
-const Stack = createStackNavigator()
+const Stack = createNativeStackNavigator()
 
 export default function HomeStackNavigator() {
   return (
     <Stack.Navigator
-      screenOptions={{ headerShown: false }}
-    >
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.SlideFromRightIOS
+    }} >
       <Stack.Screen name="HomeMain" component={HomeScreen} />
       <Stack.Screen name="Numbers" component={NumbersScreen} />
       {/* Plus tard : ajouterez ici Words, Cards, Binary, etc. */}
